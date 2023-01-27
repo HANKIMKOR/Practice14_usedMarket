90ccf119b1ad794edee200d4b7e32d05e35320d5
HANKIMKOR <95111895+HANKIMKOR@users.noreply.github.com>
2023-01-27 18:47:00 +0900

    [1] xml making

--- app/src/main/java/com/example/practice14_usedmarket/chatlist/ChatListFragment.kt
+++ app/src/main/java/com/example/practice14_usedmarket/chatlist/ChatListFragment.kt
@@ -3,5 +3,5 @@ package com.example.practice14_usedmarket.chatlist
 import androidx.fragment.app.Fragment
 import com.example.practice14_usedmarket.R
 
-class ChatListFragment : Fragment(R.layout.fragement_chatlist) {
+class ChatListFragment : Fragment(R.layout.fragment_chatlist) {
 }
--- app/src/main/java/com/example/practice14_usedmarket/home/HomeFragment.kt
+++ app/src/main/java/com/example/practice14_usedmarket/home/HomeFragment.kt
@@ -1,7 +1,23 @@
 package com.example.practice14_usedmarket.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.practice14_usedmarket.R
+import com.example.practice14_usedmarket.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment(R.layout.fragement_home) {
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var binding: FragmentHomeBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)
+        binding = fragmentHomeBinding
+
+
+
+
+    }
 }
